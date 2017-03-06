7412207ac62c98590cabca364d9668d3800c2fa1
Mridul <mridulmaharaj@gmail.com>
2017-03-06 03:09:54 +0200

    commit - 06/03/2017 - commit of latest changes

---
+++ BarChart/BarChart/BarChart.cs
@@ -0,0 +1,42 @@
+﻿// BarChart.cs
+// app that displays the output of grades as a bar chart
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarChart
+{
+    class BarChart
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+
+            Console.WriteLine("  Grade Distribution:");
+            Console.WriteLine();
+
+            for (var counter = 0; counter < array.Length; counter++)
+            {
+                if (counter == 10)
+                {
+                    Console.Write("  100: ");
+                }
+                else
+                {
+                    Console.Write($"{counter * 10:D2}-{counter * 10 + 9:D2}: ");
+                }
+
+                for(var stars = 0; stars < array[counter]; stars++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }//end Main
+    }//end class BarChart
+}
---
+++ RollDieArray/RollDieArray/RollDieArray.cs
@@ -0,0 +1,34 @@
+﻿// RollDie.cs
+// a more efficient way rolling a six sided die 60,000,000 times using an array
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollDieArray
+{
+    class RollDieArray
+    {
+        static void Main(string[] args)
+        {
+            var randomNumbers = new Random();   //random number generator
+            var frequency = new int[7];         // array frequency (counts frequency of each roll)
+            
+            for (var roll = 1; roll <= 60000000; roll++)
+            {
+                ++frequency[randomNumbers.Next(1, 7)];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"{"  Face"} {"Frequency",14} ");
+            Console.WriteLine();
+
+            for (var face = 1; face < frequency.Length; face++)
+            {
+                Console.WriteLine($"{face,5}{frequency[face],15}");
+            }
+            Console.WriteLine();
+        }//end Main
+    }//end class RollDieArray
+}
