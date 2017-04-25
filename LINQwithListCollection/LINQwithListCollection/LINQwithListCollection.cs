b4ccc8e21cf77b176b75d8bf8d41bf8d9700af89
Max452 <mridulmaharaj@gmail.com>
2017-04-25 05:22:55 +0200

    Commit 2 - 25/04/2017 - using LINQ to Objects with a generic List collection

---
+++ LINQwithListCollection/LINQwithListCollection/LINQwithListCollection.cs
@@ -0,0 +1,72 @@
+﻿// LINQwithListCollection.cs
+// LINQ to Objexts using a List<string>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQwithListCollection
+{
+    class LINQwithListCollection
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<string>();
+            items.Add("aQua");
+            items.Add("RusT");
+            items.Add("yElLow");
+            items.Add("rEd");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            //display the initial list
+            Console.Write("  items contains: ");
+            foreach(var item in items)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+
+            //converts to uppercase, selects those starting with R, and sort the list
+            var startsWithR =
+                from item in items
+                let upperCaseString = item.ToUpper()
+                where upperCaseString.StartsWith("R")
+                orderby upperCaseString
+                select upperCaseString;
+
+            Console.Write("  Results of the query startsWithR: ");
+            foreach(var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+
+            items.Add("rUbY");
+            items.Add("SaFFRon");
+
+            Console.WriteLine();
+            //display list items again
+            Console.Write("  items contains: ");
+            foreach(var item in items)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("  Results of query startsWithR: ");
+            foreach(var item in startsWithR)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }//end main
+    }//end class
+}//end namespace
