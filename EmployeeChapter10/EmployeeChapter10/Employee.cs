a56b2f2669d19e66fc11109440fed4650f733fd1
Max452 <mridulmaharaj@gmail.com>
2017-08-19 04:44:51 +0200

    Commit - 19/08/2017 - Use of static variables - Employee and EmployeeTest.cs - Classes and Objects - A deeper look

---
+++ EmployeeChapter10/EmployeeChapter10/Employee.cs
@@ -0,0 +1,31 @@
+﻿// Employee.cs - Chapter 10 - Classes and Objects - A deeper look
+// static property used to maintain a count of the number of 
+// Employee objects that have been created in the employee class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeChapter10
+{
+    public class Employee
+    {
+      public static int Count { get; private set; }     // objects in memory
+
+      public string FirstName { get; }
+      public string LastName { get; }
+
+      // intialize employee, add 1 to the static variable Count 
+      // and output a string indicating that the constructor was called
+      public Employee(string firstName, string lastName)
+      {
+            FirstName = firstName;
+            LastName = lastName;
+            ++Count;    // increment the static count of employees
+            Console.WriteLine("  Employee Constructor: " +
+                $"{FirstName} {LastName}; Count = {Count}");
+      }
+
+    }//end class Employee
+}//end namespace 
---
+++ EmployeeChapter10/EmployeeChapter10/EmployeeTest.cs
@@ -0,0 +1,39 @@
+﻿// EmployeeTest.cs - Chapter 10 - Classes and Objects: A Deeper look
+// static member demonstration
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeChapter10
+{
+    class EmployeeTest
+    {
+        static void Main()
+        {
+            // show that count variable is zero before creating any employees
+            Console.WriteLine($"\n  Employees before instantiation: {Employee.Count}");
+
+            // Create two employees; Count then becomes 2 
+            var e1 = new Employee("Susan", "Sarandon");
+            var e2 = new Employee("Bob", "Buffet");
+
+            // show that the Count variable is incremented to 2 after creating 2 employees
+            Console.WriteLine($"\n  Employees after instantiation: {Employee.Count}");
+
+            // get the names of Employees
+            Console.WriteLine($"\n  Employee 1: {e1.FirstName} {e1.LastName}");
+            Console.WriteLine($"  Employee 2: {e2.FirstName} {e2.LastName}");
+
+            // since there is only one reference to ecah Employee, the following statements
+            // cause the CLR (Common Language Runtime) to mark each Employee object as being
+            // eligible for garbage collection.
+            Console.WriteLine();
+            Console.WriteLine();
+            e1 = null;      // marks object referenced by e1 as no longer needed
+            e2 = null;      // marks object referenced by e2 as no longer needed
+
+        }//end Main
+    }//end class
+}//end namespace 
